0eb60a847b6f27c4621b4500443a0f086a58f95b
lexicilia <131444415+lexicilia@users.noreply.github.com>
2024-05-16 16:35:13 +0000

    buinn ná tengja myndina við database og setja hana inn
    buinn ná tengja myndina við database og setja hana inn

--- JobHunters/company/forms/company_form.py
+++ JobHunters/company/forms/company_form.py
@@ -1,25 +1,25 @@
-from django.forms import ModelForm, widgets
+from django import forms
 from company.models import Company
 
-class CompanyUpdateForm(ModelForm):
+
+
+
+class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
-        exclude = ['id']
+        fields = ['name', 'year_of_start', 'logo']  # Only include fields that actually exist in your model
         widgets = {
-            'name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'category': widgets.Select(attrs={'class': 'form-control'}),
-            'company': widgets.Select(attrs={'class': 'form-control'}),
-            'date_expired': widgets.DateInput(attrs={'class': 'form-control'})
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'year_of_start': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            # 'logo' does not need a widget definition since the default is usually fine, but you can specify if needed.
         }
-class CompanyForm(ModelForm):
+
+class CompanyUpdateForm(forms.ModelForm):
     class Meta:
         model = Company
-        exclude = ['id']
+        fields = ['name', 'year_of_start', 'logo']  # Adjusted to match the actual model
         widgets = {
-            'name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'category': widgets.Select(attrs={'class': 'form-control'}),
-            'company': widgets.Select(attrs={'class': 'form-control'}),
-            'date_expired': widgets.DateInput(attrs={'class': 'form-control'})
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'year_of_start': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            # Same note on 'logo' as above
         }
--- JobHunters/company/urls.py
+++ JobHunters/company/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # localhost:8000/jobs
@@ -7,4 +9,4 @@ urlpatterns = [
     path('delete_company/<int:id>', views.delete_company, name='delete-company'),
     path('update_company/<int:id>', views.update_company, name='update-company'),
     path('<int:id>', views.get_company_by_id, name='get-company'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
