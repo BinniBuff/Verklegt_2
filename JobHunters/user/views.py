4fca06d6c8821666b855c077e5b4567ffb77943c
helgzen <143876664+helgzen@users.noreply.github.com>
2024-05-16 12:01:03 +0000

    setti inn profile, reqister og login

--- JobHunters/user/views.py
+++ JobHunters/user/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from .forms import ProfileForm
+from django.contrib.auth.models import User
+from .models import Profile
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            profile = Profile.objects.create(user=user)
             return redirect('login')
-    return render(request, 'user/register.html', {'form': UserCreationForm()
-        })
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html', {'form': form})
