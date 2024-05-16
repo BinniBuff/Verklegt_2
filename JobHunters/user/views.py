89e8b164d6a8b4062bb09cf300619277fe220838
lexicilia <131444415+lexicilia@users.noreply.github.com>
2024-05-16 17:48:03 +0000

    Revert "Merge branch 'Nani'"
    This reverts commit e0d6f18d85ca3814fb1b5b797d3fcee743166d81, reversing
    changes made to 0eb60a847b6f27c4621b4500443a0f086a58f95b.

--- JobHunters/user/views.py
+++ JobHunters/user/views.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .forms import ProfileForm
-from django.contrib.auth.models import User
-from .models import Profile
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(data=request.POST)
         if form.is_valid():
-            user = form.save()
-            profile = Profile.objects.create(user=user)
+            form.save()
             return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'user/register.html', {'form': form})
+    return render(request, 'user/register.html', {'form': UserCreationForm()
+        })
