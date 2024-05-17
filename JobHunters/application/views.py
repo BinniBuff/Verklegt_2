32909a6a833cfe4a3bb7898573f8b644fe838370
BinniBuff <innibinni@gmail.com>
2024-05-17 10:22:25 +0000

    lagað views til að html virki

--- JobHunters/application/views.py
+++ JobHunters/application/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Job, JobApplication
-from .forms import ContactInformationForm, CoverLetterForm, ExperienceForm, ReferencesForm
+from forms.application_form import ContactInformationForm, CoverLetterForm, ExperienceForm, ReferencesForm
 
 # Create your views here.
 
@@ -23,7 +23,7 @@ def apply_step_1(request, job_id):
     else:
         form = ContactInformationForm()
 
-    return render(request, 'apply_step_1.html', {'form': form, 'job': job})
+    return render(request, 'application/apply_step_1.html', {'form': form, 'job': job})
 
 
 @login_required
@@ -39,7 +39,7 @@ def apply_step_2(request, job_id):
     else:
         form = CoverLetterForm(instance=application)
 
-    return render(request, 'apply_step_2.html', {'form': form, 'job': job})
+    return render(request, 'application/apply_step_2.html', {'form': form, 'job': job})
 
 
 @login_required
@@ -55,7 +55,7 @@ def apply_step_3(request, job_id):
     else:
         form = ExperienceForm(instance=application)
 
-    return render(request, 'apply_step_3.html', {'form': form, 'job': job})
+    return render(request, 'application/apply_step_3.html', {'form': form, 'job': job})
 
 
 @login_required
@@ -73,15 +73,15 @@ def apply_step_4(request, job_id):
     else:
         form = ReferencesForm(instance=application)
 
-    return render(request, 'apply_step_4.html', {'form': form, 'job': job})
+    return render(request, 'application/apply_step_4.html', {'form': form, 'job': job})
 
 
 @login_required
 def application_status(request, job_id):
     job = get_object_or_404(Job, id=job_id)
     application = get_object_or_404(JobApplication, user=request.user, job=job)
-    return render(request, 'application_status.html', {'application': application, 'job': job})
+    return render(request, 'application/application_status.html', {'application': application, 'job': job})
 
 
 def application_success(request):
-    return render(request, 'application_success.html')
+    return render(request, 'application/application_success.html')
