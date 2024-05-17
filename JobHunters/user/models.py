d119023a0e4734f6d31adc491bd702f15913760a
helgzen <143876664+helgzen@users.noreply.github.com>
2024-05-17 13:47:22 +0000

    myndogfl

--- JobHunters/user/models.py
+++ JobHunters/user/models.py
@@ -1,14 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return self.user.username + ' Profile'
+        return self.user.username
 
 
 
